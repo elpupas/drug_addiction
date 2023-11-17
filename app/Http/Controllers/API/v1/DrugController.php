fedf35fb1a656cf8fad9b31d5a800dbe89f98ed9
dustisme <androsrivas@outlook.com>
2023-11-17 19:55:25 +0100

    create models and controllers for Advice, DailyEntry, Drug, Faq, and Risk

---
+++ app/Http/Controllers/API/v1/DrugController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\API\v1;
+
+use App\Models\Drug;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class DrugController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Drug $drug)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Drug $drug)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Drug $drug)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/API/v1/RiskController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\API\v1;
+
+use App\Models\Risk;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class RiskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Risk $risk)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Risk $risk)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Risk $risk)
+    {
+        //
+    }
+}
